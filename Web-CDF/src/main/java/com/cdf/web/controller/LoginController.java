96fa810410f1d8ed2d9885a54021a7ddc308da8f
Matias Paez Molina <paezmolina@gmail.com>
2013-09-14 20:17:30 -0300

    Adjustemnt.

--- Web-CDF/src/main/java/com/cdf/web/controller/LoginController.java
+++ Web-CDF/src/main/java/com/cdf/web/controller/LoginController.java
@@ -4,23 +4,23 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cdf.dao.entity.Username;
+import com.cdf.web.form.LoginForm;
 import com.cdf.web.service.UserService;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
-public class HomeController {
+public class LoginController {
 
 	@Autowired
 	private UserService userService;
@@ -30,29 +30,37 @@ public class HomeController {
 	}
 
 	private static final Logger logger = LoggerFactory
-			.getLogger(HomeController.class);
+			.getLogger(LoginController.class);
 
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView home(Locale locale, Model model) {
+	@RequestMapping(value = "/loginForm", method = RequestMethod.GET)
+	public ModelAndView login_GET(Locale locale) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		ModelAndView mav = new ModelAndView();
-		
+
 		Username username = userService.loadUserByUsername(31);
 		mav.addObject("username", username);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
 				DateFormat.LONG, locale);
 
 		String formattedDate = dateFormat.format(date);
 
-		mav.setViewName("home");
-		//model.addAttribute("serverTime", formattedDate);
-		//model.addAttribute("username", username);
-		
+		mav.setViewName("login");
+		mav.addObject("serverTime", formattedDate);
+		mav.addObject("username", username);
+
+		return mav;
+	}
+
+	@RequestMapping(value = "loginForm", method = RequestMethod.POST)
+	public ModelAndView login_POST(@Valid LoginForm loginForm,
+			BindingResult errors) {
+		// ...handle form...
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("redirect:/profileGet");
+		System.out.println("aaaaaaaa " + loginForm.getUsername());
+
 		return mav;
 	}
 
---
+++ Web-CDF/src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,21 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body>
+<h1>
+	Login2
+</h1>
+
+<P>  The time on the server is ${serverTime}. </P>
+<P>  The time on the server is ${username.username}. </P>
+<form action="loginForm" method="post">
+   <input name="username" value="john" />
+   <input name="password" value="pass" />
+   <input type="submit"/>
+</form>
+
+</body>
+</html>
